2d516eb4d26f747fb4189aed59f3bb65ba3433e9
John Doan <johndoan42@gmail.com>
2020-06-17 20:28:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,17 +7,17 @@ sub = num1 - num2
 end
 
 def division(num1, num2)
-
+div = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+mul = num1 * num2
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
 end
 
 def square_root(num)
-
+sqrt = num**(0.5)
 end
